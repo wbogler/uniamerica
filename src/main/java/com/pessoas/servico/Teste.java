d58e7b838f579c23a129a5e319aed796768e827e
Willian Bogler <willianbogler@hotmail.com>
2026-05-04 20:40:09 -0300

    implementação da classe teste

---
+++ src/main/java/com/pessoas/servico/Teste.java
@@ -0,0 +1,18 @@
+package com.pessoas.servico;
+
+public class Teste {
+
+    private String teste;
+
+    public Teste(String teste) {
+        this.teste = teste;
+    }
+
+    public String getTeste() {
+        return teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
+}
